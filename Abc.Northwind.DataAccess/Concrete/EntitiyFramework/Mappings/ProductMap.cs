b5ba1a48ba4c2e92d030712ef9aee3abc037a07e
aniluzunoglu <aniluzunoglu@outlook.com>
2021-05-10 19:39:51 +0300

    Context is configured to use mappings

--- Abc.Northwind.DataAccess/Concrete/EntitiyFramework/Mappings/ProductMap.cs
+++ Abc.Northwind.DataAccess/Concrete/EntitiyFramework/Mappings/ProductMap.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿using Abc.Northwind.Entities.Concrete;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Abc.Northwind.DataAccess.Concrete.EntitiyFramework.Mappings
 {
-    class ProductMap
+    public class ProductMap : EntityTypeConfiguration<Product>
     {
+        public ProductMap()
+        {
+            ToTable(@"Products",@"dbo");
+            HasKey(c => c.ProductId);
+
+            Property(c => c.ProductId).HasColumnName("ProductID");
+            Property(c => c.ProductName).HasColumnName("ProductName");
+            Property(c => c.QuantityPerUnit).HasColumnName("QuantityPerUnit");
+            Property(c => c.UnitPrice).HasColumnName("UnitPrice");
+            Property(c => c.UnitsInStock).HasColumnName("UnitsInStock");
+
+        }
     }
 }
