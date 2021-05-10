4410b7fa750b96489d5daa49c7b944238eb5c98f
aniluzunoglu <aniluzunoglu@outlook.com>
2021-05-10 18:50:23 +0300

    DataAccess layer implementation

---
+++ Abc.Northwind.Business/Concrete/ProductManager.cs
@@ -0,0 +1,24 @@
+﻿using Abc.Northwind.DataAccess.Abstract;
+using Abc.Northwind.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abc.Northwind.Business.Concrete
+{
+    public class ProductManager
+    {
+        private IProductDal _productDal;
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public List<Product> GetProducts()
+        {
+            return _productDal.GetList();
+        }
+    }
+}
---
+++ Abc.Northwind.DataAccess/Concrete/EntitiyFramework/EfProductDal.cs
@@ -0,0 +1,18 @@
+﻿using Abc.Northwind.DataAccess.Abstract;
+using Abc.Northwind.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abc.Northwind.DataAccess.Concrete.EntitiyFramework
+{
+    public class EfProductDal : IProductDal
+    {
+        public List<Product> GetList()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
