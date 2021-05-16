abb9ba702201d2ed52b40ae845462db12e9633dc
aniluzunoglu <aniluzunoglu@outlook.com>
2021-05-16 09:43:41 +0300

    Ef for web UI configured

---
+++ Abc.Northwind.MvcWebUI/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using Abc.Northwind.Business.Abstract;
+using Abc.Northwind.Business.Concrete;
+using Abc.Northwind.DataAccess.Concrete.EntitiyFramework;
+using Abc.Northwind.MvcWebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Abc.Northwind.MvcWebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            // TO DO: refactor IOC 
+            IProductService productService = new ProductManager(new EfProductDal());
+            var model = new ProductListViewModel()
+            {
+                Products = productService.GetAll()
+            };
+
+            return View(model);
+        }
+    }
+}
