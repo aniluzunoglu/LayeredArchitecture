5123dd9d7c61ad66e2a5176fcc1301e9ea40a175
aniluzunoglu <aniluzunoglu@outlook.com>
2021-05-16 12:44:55 +0300

    Implementing Web API using IoC

---
+++ Abc.Northwind.WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using Abc.Northwind.Business.Abstract;
+using Abc.Northwind.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Abc.Northwind.WebAPI.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        IProductService _productService;
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+        // GET: Product
+        public List<Product> Get()
+        {
+            return _productService.GetAll();
+        }
+    }
+}
